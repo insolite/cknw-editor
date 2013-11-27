a304c112370bb2cf7d8e06bd4c49c903adb6012b
rescape <a.insolite@gmail.com>
2013-11-27 19:03:47 +0200

    build scripts fixes node-webkit for windows added

--- build-tools/linux/build.sh
+++ build-tools/linux/build.sh
@@ -1,8 +1,10 @@
+#Variables
 project_name="cknw-editor"
 platform="linux"
 executable="${project_name}"
 
-dir_root=$(pwd)
+#Dir variables
+dir_root=`dirname "$(readlink -f "$0")"`
 dir_project="${dir_root}/../.."
 dir_builds="${dir_project}/build"
 dir_build="${dir_builds}/${project_name}-${platform}"
@@ -10,10 +12,14 @@ dir_build_tmp="${dir_build}/tmp"
 dir_source="${dir_project}/${project_name}"
 dir_node_webkit="${dir_project}/node-webkit/${platform}"
 
+#Initial setup
 mkdir -p ${dir_builds}
 mkdir -p ${dir_build}
 mkdir -p ${dir_build_tmp}
 
+#Project specific block begin
+
+#Generate links for new extra ckeditor plugins
 cd ${dir_source}/ckeditor/plugins
 for plugin in ${dir_source}/ckeditor-extra-plugins/*
 do
@@ -21,15 +27,21 @@ do
     ln -s ${plugin} ./$(basename ${plugin})
 done
 
+#Project specific block end
+
+#Zip application
 cd ${dir_source}
 zip -9 -r -q "${dir_build_tmp}/${project_name}.nw" ./*
 
+#Pack application with node-webkit
 cat ${dir_node_webkit}/nw ${dir_build_tmp}/${project_name}.nw > ${dir_build}/${executable}
 chmod +x ${dir_build}/${project_name}
 cp ${dir_node_webkit}/nw.pak ${dir_build}
 
+#Clean up
 rm -rf ${dir_build_tmp}
 
+#Run application
 run="${dir_build}/${executable}"
 args="/home/oleg/projects/emergency_sunpp"
 ${run} ${args}
--- build-tools/windows/build.bat
+++ build-tools/windows/build.bat
@@ -1,43 +1,54 @@
-set /p project_name="cknw-editor"
-set /p platform="windows"
-set /p executable="%%project_name"
+::Variables
+set project_name=cknw-editor
+set platform=windows
+set executable=%project_name%.exe
 
-set dir_build_tools=%CD%
-set /p dir_project="%%dir_build_tools\..\.."
-set /p dir_builds="%%dir_project\build"
-set /p dir_build="%%dir_builds\%%project_name-%%platform"
-set /p dir_build_tmp="%%dir_build\tmp"
-set /p dir_source="%%dir_project\%%project_name"
-set /p dir_node_webkit="%%dir_project\node-webkit\%%platform"
+::Dir variables
+set dir_build_tools=%~dp0
+set dir_project=%dir_build_tools%\..\..
+set dir_builds=%dir_project%\build
+set dir_build=%dir_builds%\%project_name%-%platform%
+set dir_build_tmp=%dir_build%\tmp
+set dir_source=%dir_project%\%project_name%
+set dir_node_webkit=%dir_project%\node-webkit\%platform%
 
-mkdir %%dir_builds
-mkdir %%dir_build
-mkdir %%dir_build_tmp
+::Initial setup
+mkdir %dir_builds%
+mkdir %dir_build%
+mkdir %dir_build_tmp%
+
+::Project specific block begin
 
 ::Generate links for new extra ckeditor plugins
-cd %%dir_source\ckeditor\plugins
-for /f %%plugin in ('dir /b %%dir_source\ckeditor-extra-plugins') do (
-	del %%plugin
-	mklink /d %%plugin %%dir_source\ckeditor-extra-plugins\%%plugin
+cd %dir_source%\ckeditor\plugins
+for /f %plugin% in ('dir /b %dir_source%\ckeditor-extra-plugins') do (
+	del %plugin%
+	mklink /d %plugin% %dir_source%\ckeditor-extra-plugins\%plugin%
 )
 
 ::Resolve links (required for windows only)
-echo D | xcopy /s /e /q %%dir_source %%dir_build_tmp
-cd %%dir_build_tmp\%%dir_source\ckeditor-extra-plugins
-for /f %%plugin in ('dir /b') do (
-	del %%dir_build_tmp\%%dir_source\ckeditor\plugins\%%plugin
-	move %%plugin %%dir_build_tmp\%%dir_source\ckeditor\plugins
+echo D | xcopy /s /e /q %dir_source% %dir_build_tmp%
+cd %dir_build_tmp%\%dir_source%\ckeditor-extra-plugins
+for /f %plugin% in ('dir /b') do (
+	del %dir_build_tmp%\%dir_source%\ckeditor\plugins\%plugin%
+	move %plugin% %dir_build_tmp%\%dir_source%\ckeditor\plugins
 )
 
+::Project specific block end
+
 ::Zip application
-cd %%dir_build_tmp\%%dir_source
-%%dir_build_tools\7za.exe a -tzip %%dir_buil_tmp\ckeditor.zip *
+cd %dir_build_tmp%\%dir_source%
+%dir_build_tools%\7za.exe a -tzip %dir_buil_tmp%\ckeditor.nw *
 
 ::Pack application with node-webkit
-copy /b %%dir_node_webkit\nw.exe+%%dir_build_tmp\%%project_name.nw %%dir_build\%%executable
-copy %%dir_node_webkit\nw.pak %%dir_build
+copy /b %dir_node_webkit%\nw.exe+%dir_build_tmp%\%project_name%.nw %dir_build%\%executable%
+copy %dir_node_webkit%\nw.pak %dir_build%
 
-set /p run="%%dir_build\%%executable"
-set /p args="c:\Users\oleg\My Documents\projects\emergency_sunpp"
-%%run %%args
+::Clean up
+rmdir /s /e %dir_build_tmp%
+
+::Run application
+set run=%dir_build%\%executable%
+set args=c:\Users\oleg\My Documents\projects\emergency_sunpp
+%run% %args%
 

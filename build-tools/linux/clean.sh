a7822c872f8a56c6d49a9fb51a61cd6090e3c593
rescape <a.insolite@gmail.com>
2013-11-28 20:12:20 +0200

    changes in clean script

--- build-tools/linux/clean.sh
+++ build-tools/linux/clean.sh
@@ -4,8 +4,8 @@ platform="linux"
 executable="${project_name}"
 
 #Dir variables
-dir_root=`dirname "$(readlink -f "$0")"`
-dir_project="${dir_root}/../.."
+dir_build_tools=`dirname "$(readlink -f "$0")"`
+dir_project="${dir_build_tools}/../.."
 dir_builds="${dir_project}/build"
 dir_build="${dir_builds}/${project_name}-${platform}"
 
